7def4cdf3a9f71fabe365df47b3b9c2b0ce30540
Talha Momin <77355191+momintlh@users.noreply.github.com>
2025-06-06 20:35:03 +0500

    initial setup

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
 import * as THREE from 'three'
+
+const canvas = document.querySelector('canvas.webgl');
+
+const sizes = {
+  width: 500,
+  height: 500
+};
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+camera.position.z = 5;
+
+const boxMesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+
+scene.add(boxMesh);
+
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
