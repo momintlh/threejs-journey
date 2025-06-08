ef27fec099d555bb6f473161129170bc60db33c2
Talha Momin <77355191+momintlh@users.noreply.github.com>
2025-06-08 21:07:21 +0500

    setup

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
 import * as THREE from 'three'
+
+const sizes = {
+    width: 800,
+    height: 600,
+}
+const aspectRatio = sizes.width / sizes.height
+
+const canvas = document.querySelector("canvas.webgl");
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+camera.position.z = 2;
+const boxMesh = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({ color: 0xFFFF00 }))
+
+
+scene.add(camera);
+scene.add(boxMesh);
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera);
